2af47578eeb4cb3a38f7626c7dbbd41134a661d5
Alexander Antonov <alexandr-post@yandex.ru>
2016-11-07 21:59:59 +0700

    task A + B

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    let result;
+    const {a = 0, b = 0} = req.query;
+
+    result = Number(a) + Number(b);
+
+    res.send(String(result));
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+})
